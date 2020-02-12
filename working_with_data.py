fa2ef8b4268c363d0635011481e50dfbf10e279a
brown5628 <brown5628@gmail.com>
2020-02-12 07:32:08 -0500

    Continue ch 10

--- working_with_data.py
+++ working_with_data.py
@@ -38,3 +38,79 @@ plot_histogram(uniform,10, "Uniform Hist")
 plot_histogram(normal, 10, 'Normal Hist')
 
 # %%
+def random_normal() -> float:
+    """Returns a random draw from a standard normal distribution"""
+    return inverse_normal_cdf(random.random())
+
+xs = [random_normal() for _ in range(1000)]
+ys1 = [ x + random_normal() / 2 for x in xs]
+ys2 = [-x + random_normal() / 2 for x in xs]
+
+# %%
+plt.scatter(xs, ys1, marker='.', color='black', label='ys1')
+plt.scatter(xs, ys2, marker='.', color='gray', label='ys2')
+plt.xlabel('xs')
+plt.ylabel('ys')
+plt.legend(loc=9)
+plt.title("Very Different Joint Distributions")
+plt.show()
+
+# %%
+from statisticss import correlation
+
+print(correlation(xs, ys1))
+print(correlation(xs, ys2))
+
+# %%
+from linear_algebra import Matrix, Vector, make_matrix
+
+def correlation_matrix(data: List[Vector]) -> Matrix:
+    """
+    Returns the len(data) x len(data) matrix whose (i, j)-th entry is the correlation between data[i] and data[j]
+    """
+    def correlation_ij(i: int, j:int) -> float:
+        return correlation(data[i], data[j])
+    
+    return make_matrix(len(data), len(data), correlation_ij)
+# %%
+from typing import List
+
+# Just some random data to show off correlation scatterplots
+num_points = 100
+
+def random_row() -> List[float]:
+    row = [0.0, 0, 0, 0]
+    row[0] = random_normal()
+    row[1] = -5 * row[0] + random_normal()
+    row[2] = row[0] + row[1] + 5 * random_normal()
+    row[3] = 6 if row[2] > -2 else 0
+    return row
+
+corr_rows = [random_row() for _ in range(num_points)]
+
+corr_data = [list(col) for col in zip(*corr_rows)]
+# %%
+# corr_data is a list of four 100-d vectors
+num_vectors = len(corr_data)
+fig, ax = plt.subplots(num_vectors, num_vectors)
+
+for i in range(num_vectors):
+    for j in range(num_vectors):
+
+        # Scatter column_j on the x-axis vs. column_i on the y-axis
+        if i != j: ax[i][j].scatter(corr_data[j], corr_data[i])
+
+        # unless i == j, in which case show the series name
+        else: ax[i][j].annotate("series " + str(i), (.5, .5), xycoords='axes fraction', ha="center", va="center")
+
+        # Then hide axis labels except left and bottom charts
+        if i < num_vectors - 1: ax[i][j].xaxis.set_visible(False)
+        if j > 0: ax[i][j].yaxis.set_visible(False)
+
+# fix the bottom-right and top left axis labels, which are off because only contain text
+ax[-1][-1].set_xlim(ax[0][-1].get_xlim())
+ax[0][0].set_ylim(ax[0][1].get_ylim())
+
+plt.show()
+
+# %%
